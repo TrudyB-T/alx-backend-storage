3a87005e5bd77737ac0bedebe7e0743b7812a0b8
TrudyB-T <drtrudybannerman@gmail.com>
2024-02-27 18:01:29 +0000

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,7 +7,7 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
  """change all topics """
-    return mongo_collection.update_many(
+ return mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
