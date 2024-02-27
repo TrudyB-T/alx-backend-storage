5b8d206baf6dfa35cf9da48d219c7720d7ab90d3
TrudyB-T <128814293+TrudyB-T@users.noreply.github.com>
2024-02-27 17:53:44 +0000

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,7 @@ import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
+ """change all topics """
     return mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
